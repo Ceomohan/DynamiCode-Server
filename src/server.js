d932dba68336f9a25998d89e7438756d5034e1b7
Ceomohan <ceomohan2004@gmail.com>
2026-01-24 17:43:34 +0530

    Port number modified

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = require('./app');
 // Connect to database
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
